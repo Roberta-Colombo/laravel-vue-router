5e11b436511ebe728ece3af963133882f488ee58
Roberta-Colombo <roberta.colombo89@hotmail.it>
2023-01-18 16:03:53 +0100

    Added store.js + api call

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -3,8 +3,28 @@
 </template>
 
 <script>
+import axios from "axios";
+import { store } from "../store";
+
 export default {
   name: "ProjectList",
+  data() {
+    return {
+      store,
+      projects: [],
+    };
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.store.apiBaseUrl}/projects`).then((response) => {
+        console.log(response.data.results);
+        this.projects = response.data.results.data;
+      });
+    },
+  },
+  mounted() {
+    this.getProjects();
+  },
 };
 </script>
 
