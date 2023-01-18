6bfa65542e290edaf3718500aa5ab97a7a45d196
Roberta-Colombo <roberta.colombo89@hotmail.it>
2023-01-18 16:44:27 +0100

    Added ProjectCard - no style - wip

---
+++ src/pages/ProjectCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      :src="`${store.imgPath}${project.image_1}`"
+      class="card-img-top"
+      :alt="project.title"
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <p class="card-text">
+        {{ project.description }}
+      </p>
+
+      <router-link
+        class="btn btn-primary"
+        :to="{ name: 'single-project', params: { slug: project.slug } }"
+      >
+        See more
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from "../store";
+
+export default {
+  name: "ProjectCard",
+  props: {
+    project: Object,
+  },
+  data() {
+    return {
+      store,
+      contentMaxLen: 100,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>lista progetti</div>
+  <h1>lista progetti</h1>
+
+  <div class="row">
+    <div class="col-12 col-md-4" v-for="(item, index) in projects" :key="index">
+      <ProjectCard :project="item"></ProjectCard>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import { store } from "../store";
+import ProjectCard from "./ProjectCard.vue";
 
 export default {
   name: "ProjectList",
@@ -14,11 +21,12 @@ export default {
       projects: [],
     };
   },
+  components: { ProjectCard },
   methods: {
     getProjects() {
       axios.get(`${this.store.apiBaseUrl}/projects`).then((response) => {
-        console.log(response.data.results);
-        this.projects = response.data.results.data;
+        // console.log(response.data.results);
+        this.projects = response.data.results;
       });
     },
   },
